580a9ed88df5227ee5a89c149c1b4c8c96fd6870
RobinBruneel <25rbruneel@gmail.com>
2018-09-06 11:02:11 +0200

    use template instead of html file

--- src/pages/opening-hours-management/opening-hours-management.ts
+++ src/pages/opening-hours-management/opening-hours-management.ts
@@ -5,7 +5,65 @@ import {OpeningHoursFacade} from "../facade/opening-hours.facade";
 
 @Component({
   selector: 'page-opening-hours-management',
-  templateUrl: './opening-hours-management.html'
+  template: `<ion-header>
+    <ion-navbar>
+      <ion-title>
+        Openingsuren
+      </ion-title>
+    </ion-navbar>
+  </ion-header>
+
+  <ion-content padding>
+    <ion-card padding *ngFor="let day of facade.days">
+      <ion-row align-items-center (click)="day.toggleOpen()">
+        <ion-label style="width: 100px;">{{day.dayName | titlecase}}</ion-label>
+        <ion-list float-right>
+          <ion-item *ngFor="let slot of day.timeSlots">
+            <ion-label no-margin no-padding>
+              {{slot.getFormattedHours()}}
+            </ion-label>
+          </ion-item>
+        </ion-list>
+        <ion-icon [name]="displayIcon(day)" color="favorite" item-right
+                  float-right
+                  style="margin-left: 8px;"></ion-icon>
+      </ion-row>
+      <ion-card-content *ngIf="day.open">
+        <ion-row style="max-width: 300px;" *ngFor="let timeslot of day.timeSlots">
+          <ion-col>
+            <ion-datetime displayFormat="HH mm" placeholder="Openingsuur"
+                          [(ngModel)]="timeslot.openingsHour"></ion-datetime>
+          </ion-col>
+          <ion-col>
+            <p style="text-align: center;">-</p>
+          </ion-col>
+          <ion-col>
+            <ion-datetime displayFormat="HH mm" placeholder="Sluitingsuur"
+                          [(ngModel)]="timeslot.closingHour"></ion-datetime>
+          </ion-col>
+          <ion-col>
+            <button ion-button clear (click)="day.deleteTimeSlot(timeslot)">
+              <ion-icon name="close-circle"></ion-icon>
+            </button>
+          </ion-col>
+        </ion-row>
+
+        <ion-row align-items-center>
+          <button clear ion-button (click)="addHourSlot(day)" [disabled]="day.isOpenFor24hours">Voeg tijden toe</button>
+        </ion-row>
+
+        <ion-row>
+          <ion-item>
+            <ion-label>Is 24 uur open</ion-label>
+            <ion-checkbox [(ngModel)]="day.isOpenFor24hours"></ion-checkbox>
+          </ion-item>
+        </ion-row>
+
+      </ion-card-content>
+    </ion-card>
+
+  </ion-content>
+  `
 })
 export class OpeningHoursManagementPage {
 
--- src/pages/opening-hours/opening-hours.ts
+++ src/pages/opening-hours/opening-hours.ts
@@ -12,7 +12,27 @@ import {OpeningHoursFacade} from "../facade/opening-hours.facade";
 @IonicPage()
 @Component({
   selector: 'page-opening-hours',
-  templateUrl: './opening-hours.html',
+  template: `
+    <ion-header>
+
+      <ion-navbar>
+        <ion-title>opening-hours</ion-title>
+      </ion-navbar>
+
+    </ion-header>
+
+
+    <ion-content padding>
+      <ion-row *ngFor="let day of facade.days" style="border-bottom: lightgray 1px solid" >
+        <ion-label style="width: 50px;">{{day.dayName}}</ion-label>
+        <ion-col>
+          <ion-row *ngFor="let timeSlot of day.timeSlots">
+            <ion-label text-right>{{timeSlot.getFormattedHours()}}</ion-label>
+          </ion-row>
+        </ion-col>
+      </ion-row>
+    </ion-content>
+  `,
 })
 export class OpeningHoursPage {
 
